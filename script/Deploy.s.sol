ca7c8158bde34d83bf0ed9ab802f35395f0c618d
ZeframLou <zeframlou@gmail.com>
2022-12-08 16:42:12 -0800

    feat: use CREATE3Script for deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,14 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {CREATE3Factory} from "create3-factory/CREATE3Factory.sol";
-
-import "forge-std/Script.sol";
-
 import {LSSVMPair} from "../src/LSSVMPair.sol";
 import {LSSVMRouter} from "../src/LSSVMRouter.sol";
 import {LSSVMRouter2} from "../src/LSSVMRouter2.sol";
 import {LSSVMPairETH} from "../src/LSSVMPairETH.sol";
+import {CREATE3Script} from "./base/CREATE3Script.sol";
 import {LSSVMPairERC20} from "../src/LSSVMPairERC20.sol";
 import {XykCurve} from "../src/bonding-curves/XykCurve.sol";
 import {LSSVMPairFactory} from "../src/LSSVMPairFactory.sol";
@@ -16,7 +13,9 @@ import {LinearCurve} from "../src/bonding-curves/LinearCurve.sol";
 import {ExponentialCurve} from "../src/bonding-curves/ExponentialCurve.sol";
 import {LSSVMRouterWithRoyalties} from "../src/LSSVMRouterWithRoyalties.sol";
 
-contract DeployScript is Script {
+contract DeployScript is CREATE3Script {
+    constructor() CREATE3Script(vm.envString("VERSION")) {}
+
     function run()
         external
         returns (
@@ -30,9 +29,6 @@ contract DeployScript is Script {
         )
     {
         uint256 deployerPrivateKey = uint256(vm.envBytes32("PRIVATE_KEY"));
-        CREATE3Factory create3 = CREATE3Factory(
-            0x9fBB3DF7C40Da2e5A0dE984fFE2CCB7C47cd0ABf
-        );
 
         uint256 protocolFee = vm.envUint("PROTOCOL_FEE");
         address protocolFeeRecipient = vm.envAddress("PROTOCOL_FEE_RECIPIENT");
@@ -42,33 +38,19 @@ contract DeployScript is Script {
         // deploy factory
         {
             LSSVMPairETH ethTemplate = LSSVMPairETH(
-                payable(
-                    create3.deploy(
-                        keccak256("LSSVMPairETH"),
-                        type(LSSVMPairETH).creationCode
-                    )
-                )
+                payable(create3.deploy(getCreate3ContractSalt("LSSVMPairETH"), type(LSSVMPairETH).creationCode))
             );
             LSSVMPairERC20 erc20Template = LSSVMPairERC20(
-                create3.deploy(
-                    keccak256("LSSVMPairERC20"),
-                    type(LSSVMPairERC20).creationCode
-                )
+                create3.deploy(getCreate3ContractSalt("LSSVMPairERC20"), type(LSSVMPairERC20).creationCode)
             );
             address deployer = vm.addr(deployerPrivateKey);
             factory = LSSVMPairFactory(
                 payable(
                     create3.deploy(
-                        keccak256("LSSVMPairFactory"),
+                        getCreate3ContractSalt("LSSVMPairFactory"),
                         bytes.concat(
                             type(LSSVMPairFactory).creationCode,
-                            abi.encode(
-                                ethTemplate,
-                                erc20Template,
-                                protocolFeeRecipient,
-                                protocolFee,
-                                deployer
-                            )
+                            abi.encode(ethTemplate, erc20Template, protocolFeeRecipient, protocolFee, deployer)
                         )
                     )
                 )
@@ -76,21 +58,11 @@ contract DeployScript is Script {
         }
 
         // deploy bonding curves
-        linearCurve = LinearCurve(
-            create3.deploy(
-                keccak256("LinearCurve"),
-                type(LinearCurve).creationCode
-            )
-        );
+        linearCurve = LinearCurve(create3.deploy(getCreate3ContractSalt("LinearCurve"), type(LinearCurve).creationCode));
         exponentialCurve = ExponentialCurve(
-            create3.deploy(
-                keccak256("ExponentialCurve"),
-                type(ExponentialCurve).creationCode
-            )
+            create3.deploy(getCreate3ContractSalt("ExponentialCurve"), type(ExponentialCurve).creationCode)
         );
-        xykCurve = XykCurve(
-            create3.deploy(keccak256("XykCurve"), type(XykCurve).creationCode)
-        );
+        xykCurve = XykCurve(create3.deploy(getCreate3ContractSalt("XykCurve"), type(XykCurve).creationCode));
 
         // whitelist bonding curves
         factory.setBondingCurveAllowed(linearCurve, true);
@@ -101,33 +73,24 @@ contract DeployScript is Script {
         router = LSSVMRouter(
             payable(
                 create3.deploy(
-                    keccak256("LSSVMRouter"),
-                    bytes.concat(
-                        type(LSSVMRouter).creationCode,
-                        abi.encode(factory)
-                    )
+                    getCreate3ContractSalt("LSSVMRouter"),
+                    bytes.concat(type(LSSVMRouter).creationCode, abi.encode(factory))
                 )
             )
         );
         router2 = LSSVMRouter2(
             payable(
                 create3.deploy(
-                    keccak256("LSSVMRouter2"),
-                    bytes.concat(
-                        type(LSSVMRouter2).creationCode,
-                        abi.encode(factory)
-                    )
+                    getCreate3ContractSalt("LSSVMRouter2"),
+                    bytes.concat(type(LSSVMRouter2).creationCode, abi.encode(factory))
                 )
             )
         );
         routerWithRoyalties = LSSVMRouterWithRoyalties(
             payable(
                 create3.deploy(
-                    keccak256("LSSVMRouterWithRoyalties"),
-                    bytes.concat(
-                        type(LSSVMRouterWithRoyalties).creationCode,
-                        abi.encode(factory)
-                    )
+                    getCreate3ContractSalt("LSSVMRouterWithRoyalties"),
+                    bytes.concat(type(LSSVMRouterWithRoyalties).creationCode, abi.encode(factory))
                 )
             )
         );
