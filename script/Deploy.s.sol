dbaaf2d6aa5bab2167056307f7033a5937b9810d
Owen <22165146+sudo-owen@users.noreply.github.com>
2023-04-06 22:21:23 -0700

    update deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -11,13 +11,21 @@ import {CREATE3Script} from "./base/CREATE3Script.sol";
 import {RoyaltyEngine} from "../src/RoyaltyEngine.sol";
 import {VeryFastRouter} from "../src/VeryFastRouter.sol";
 import {XykCurve} from "../src/bonding-curves/XykCurve.sol";
+import {GDACurve} from "../src/bonding-curves/GDACurve.sol";
 import {LSSVMPairFactory} from "../src/LSSVMPairFactory.sol";
+import {ILSSVMPairFactoryLike} from "../src/ILSSVMPairFactoryLike.sol";
 import {LinearCurve} from "../src/bonding-curves/LinearCurve.sol";
 import {LSSVMPairERC721ETH} from "../src/erc721/LSSVMPairERC721ETH.sol";
 import {LSSVMPairERC1155ETH} from "../src/erc1155/LSSVMPairERC1155ETH.sol";
 import {LSSVMPairERC721ERC20} from "../src/erc721/LSSVMPairERC721ERC20.sol";
 import {ExponentialCurve} from "../src/bonding-curves/ExponentialCurve.sol";
 import {LSSVMPairERC1155ERC20} from "../src/erc1155/LSSVMPairERC1155ERC20.sol";
+import {StandardSettings} from "../src/settings/StandardSettings.sol";
+import {StandardSettingsFactory} from "../src/settings/StandardSettingsFactory.sol";
+import {Splitter} from "../src/settings/Splitter.sol";
+import {PropertyCheckerFactory} from "../src/property-checking/PropertyCheckerFactory.sol";
+import {MerklePropertyChecker} from "../src/property-checking/MerklePropertyChecker.sol";
+import {RangePropertyChecker} from "../src/property-checking/RangePropertyChecker.sol";
 
 contract DeployScript is CREATE3Script {
     constructor() CREATE3Script(vm.envString("VERSION")) {}
@@ -30,24 +38,26 @@ contract DeployScript is CREATE3Script {
             LinearCurve linearCurve,
             ExponentialCurve exponentialCurve,
             XykCurve xykCurve,
-            RoyaltyEngine royaltyEngine
+            GDACurve gdaCurve,
+            RoyaltyEngine royaltyEngine,
+            StandardSettingsFactory settingsFactory,
+            PropertyCheckerFactory propertyCheckerFactory
         )
     {
         uint256 deployerPrivateKey = uint256(vm.envBytes32("PRIVATE_KEY"));
-
-        uint256 protocolFee = vm.envUint("PROTOCOL_FEE");
-        address protocolFeeRecipient = vm.envAddress("PROTOCOL_FEE_RECIPIENT");
         address royaltyRegistry = vm.envAddress("ROYALTY_REGISTRY");
 
         vm.startBroadcast(deployerPrivateKey);
 
         require(ERC165Checker.supportsInterface(royaltyRegistry, type(IRoyaltyRegistry).interfaceId));
-        royaltyEngine = RoyaltyEngine(
-            create3.deploy(
-                getCreate3ContractSalt("RoyaltyEngine"),
-                bytes.concat(type(RoyaltyEngine).creationCode, abi.encode(royaltyRegistry))
-            )
-        );
+
+        // royaltyEngine = RoyaltyEngine(
+        //     create3.deploy(
+        //         getCreate3ContractSalt("RoyaltyEngine"),
+        //         bytes.concat(type(RoyaltyEngine).creationCode, abi.encode(royaltyRegistry))
+        //     )
+        // );
+        royaltyEngine = RoyaltyEngine(address(0x13FAF01b9027FAe4572Ef1D3f848597174c7f3F1));
 
         // deploy factory
         bytes memory factoryConstructorArgs;
@@ -86,8 +96,8 @@ contract DeployScript is CREATE3Script {
                 erc721ERC20Template,
                 erc1155ETHTemplate,
                 erc1155ERC20Template,
-                protocolFeeRecipient,
-                protocolFee,
+                vm.envAddress("PROTOCOL_FEE_RECIPIENT"),
+                vm.envUint("PROTOCOL_FEE"),
                 deployer
             );
         }
@@ -101,16 +111,21 @@ contract DeployScript is CREATE3Script {
         );
 
         // deploy bonding curves
-        linearCurve = LinearCurve(create3.deploy(getCreate3ContractSalt("LinearCurve"), type(LinearCurve).creationCode));
-        exponentialCurve = ExponentialCurve(
-            create3.deploy(getCreate3ContractSalt("ExponentialCurve"), type(ExponentialCurve).creationCode)
-        );
-        xykCurve = XykCurve(create3.deploy(getCreate3ContractSalt("XykCurve"), type(XykCurve).creationCode));
+        // linearCurve = LinearCurve(create3.deploy(getCreate3ContractSalt("LinearCurve"), type(LinearCurve).creationCode));
+        // exponentialCurve = ExponentialCurve(
+        //     create3.deploy(getCreate3ContractSalt("ExponentialCurve"), type(ExponentialCurve).creationCode)
+        // );
+        // xykCurve = XykCurve(create3.deploy(getCreate3ContractSalt("XykCurve"), type(XykCurve).creationCode));
+        linearCurve = LinearCurve(address(0x2b876A902fe11fb6fAD01DF3ee122B9b784c9A84));
+        exponentialCurve = ExponentialCurve(address(0xcEFfc28d19878917fC933A2b4688f24810AF6F65));
+        xykCurve = XykCurve(address(0xcEc01d4e17c349c494F1acda4E411532E46F9196));
+        gdaCurve = GDACurve(create3.deploy(getCreate3ContractSalt("GDACurve"), type(GDACurve).creationCode));
 
         // whitelist bonding curves
         factory.setBondingCurveAllowed(linearCurve, true);
         factory.setBondingCurveAllowed(exponentialCurve, true);
         factory.setBondingCurveAllowed(xykCurve, true);
+        factory.setBondingCurveAllowed(gdaCurve, true);
 
         // deploy router
         router = VeryFastRouter(
@@ -125,12 +140,58 @@ contract DeployScript is CREATE3Script {
         // whitelist router
         factory.setRouterAllowed(LSSVMRouter(payable(address(router))), true);
 
+        // deploy settings factory
+        settingsFactory = deploySettingsFactory(factory);
+
+        // deploy property checker factory
+        propertyCheckerFactory = deployPropertyCheckerFactory();
+
         // transfer factory ownership
         {
             address owner = vm.envAddress("OWNER");
             factory.transferOwnership(owner);
         }
-
         vm.stopBroadcast();
+    }
+
+    function deploySettingsFactory(ILSSVMPairFactoryLike factory)
+        internal
+        returns (StandardSettingsFactory settingsFactory)
+    {
+        Splitter splitterImplementation =
+            Splitter(payable(create3.deploy(getCreate3ContractSalt("Splitter"), type(Splitter).creationCode)));
+        StandardSettings settingsImplementation = StandardSettings(
+            create3.deploy(
+                getCreate3ContractSalt("StandardSettings"),
+                bytes.concat(type(StandardSettings).creationCode, abi.encode(splitterImplementation, factory))
+            )
+        );
+        settingsFactory = StandardSettingsFactory(
+            create3.deploy(
+                getCreate3ContractSalt("StandardSettingsFactory"),
+                bytes.concat(type(StandardSettingsFactory).creationCode, abi.encode(settingsImplementation))
+            )
+        );
+    }
+
+    function deployPropertyCheckerFactory() internal returns (PropertyCheckerFactory propertyCheckerFactory) {
+        MerklePropertyChecker merklePropertyChecker = MerklePropertyChecker(
+            create3.deploy(
+                getCreate3ContractSalt("MerklePropertyChecker"), bytes.concat(type(MerklePropertyChecker).creationCode)
+            )
+        );
+        RangePropertyChecker rangePropertyChecker = RangePropertyChecker(
+            create3.deploy(
+                getCreate3ContractSalt("RangePropertyChecker"), bytes.concat(type(RangePropertyChecker).creationCode)
+            )
+        );
+        propertyCheckerFactory = PropertyCheckerFactory(
+            create3.deploy(
+                getCreate3ContractSalt("PropertyCheckerFactory"),
+                bytes.concat(
+                    type(PropertyCheckerFactory).creationCode, abi.encode(merklePropertyChecker, rangePropertyChecker)
+                )
+            )
+        );
     }
 }
