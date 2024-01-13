fd27452f81bb15812a9e54c3b81512396264602e
owen <owen@sudorandom.org>
2024-01-12 17:03:23 -0800

    downgrade req for interface

--- src/hooks/IPairHooks.sol
+++ src/hooks/IPairHooks.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.0;
 
 import {ERC20} from "solmate/tokens/ERC20.sol";
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
